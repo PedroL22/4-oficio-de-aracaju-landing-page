27a977242d065817afe1c6e4bb9aac5dfd2569e5
Pedro Lucena <pedrocontact22@gmail.com>
2025-05-24 00:44:23 -0300

    🔍️ feat: Add SEO metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,21 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: '4º Ofício de Aracaju',
-  description: '4º Ofício de Aracaju',
+  description:
+    'Serviços de Registro Civil e Tabelionato de Notas com atendimento ágil e de qualidade para a população de Aracaju.',
+  openGraph: {
+    title: '4º Ofício de Aracaju',
+    description:
+      'Serviços de Registro Civil e Tabelionato de Notas com atendimento ágil e de qualidade para a população de Aracaju.',
+    images: ['/logotype.png'],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '4º Ofício de Aracaju',
+    description:
+      'Serviços de Registro Civil e Tabelionato de Notas com atendimento ágil e de qualidade para a população de Aracaju.',
+    images: ['/logotype.png'],
+  },
 }
 
 export default function RootLayout({
